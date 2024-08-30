fc51b4d44d50bbb0fa61f6f0a8076cfa9434e9a0
Sebastian Suarez <Juan_Suarez1@ucne.edu.do>
2024-08-29 23:31:35 -0400

    Agregar archivos de proyecto.

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; } 
+        [Required(ErrorMessage = "El campo Nombres es obligatorio..")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "Es necesario especificar un sueldo x hora..")]
+        [Range (100, 5000, ErrorMessage = "El sueldo debe de estar entre 100 y 5,000...") ]
+        public int SueldoHora { get; set; }
+    }
+}
