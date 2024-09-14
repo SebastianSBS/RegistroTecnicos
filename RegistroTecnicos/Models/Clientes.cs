d2b432419412af2ed2ade4f28b18630d96df997f
Sebastian Suarez <Juan_Suarez1@ucne.edu.do>
2024-09-13 23:03:31 -0400

    Agregando las clases Clientes y Trabajos

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClientesId { get; set; }
+    [Required(ErrorMessage = "El campo Nombres es obligatorio..")]
+    public string Nombres { get; set; }
+    [Required(ErrorMessage = "El campo wasa es obligatorio..")]
+    public int WhatsApp { get; set; }
+}
---
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Trabajos
+{
+    [Key]
+    public int TrabajoId { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public int ClienteId { get; set; }
+    [ForeignKey("TecnicoId")]
+    public int TecnicoId { get; set; }
+    [Required(ErrorMessage = "El campo descripcion es necesario..")]
+    public string Descripcion { get; set; }
+    [Required(ErrorMessage = "El campo monto es necesario..")]
+    public int Monto { get; set; }
+}
